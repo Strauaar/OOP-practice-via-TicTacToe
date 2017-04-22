d18e3496fe7de19e39fe789be197ac0e4435bd3a
Aaron <astrauli@gmail.com>
2017-04-22 10:17:21 -0700

    Near Final

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,7 +21,7 @@ module TicTacToe
           expect(row).to have(3).things
         end
       end
-    end
+
 
     context "#grid" do
       it "returns the grid" do
@@ -35,40 +35,96 @@ module TicTacToe
         grid = [["","",""], ["","","something"],["","",""]]
         board = Board.new(grid: grid)
         expect(board.get_cell(2, 1)).to eq "something"
+      end
     end
+
+    context "#set_cell" do
+      it "updates the value of the cell object at a (x, y) coordinate" do
+        Cat = Struct.new(:value)
+        grid = [[Cat.new("cool"), "", ""], ["", "", ""], ["", "", ""]]
+        board = Board.new(grid: grid)
+        board.set_cell(0, 0, "cell")
+        expect(board.get_cell(0, 0).value).to eq "cell"
+      end
+    end
+
+    TestCell = Struct.new(:value)
+    let(:x_cell) { TestCell.new("X") }
+    let(:y_cell) { TestCell.new("Y") }
+    let(:empty) { TestCell.new }
+
+    context "#game_over" do
+      it "returns :winner if winner? is true" do
+        board = Board.new
+        allow(board).to receive(:winner?).and_return(true)
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :draw if winner? is false and draw? is true" do
+        board = Board.new
+        allow(board).to receive(:winner?).and_return(false)
+        allow(board).to receive(:draw?).and_return(true)
+        expect(board.game_over).to eq :draw
+      end
+
+      it "returns false if winner? is false and draw? is false" do
+        board = Board.new
+        allow(board).to receive(:winner?).and_return(false)
+        allow(board).to receive(:draw?).and_return(false)
+        expect(board.game_over).to be_falsey
+      end
+
+      it "returns :winner when row has objets with values that are all the same" do
+        grid = [
+          [x_cell, x_cell, empty],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, empty]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :winner when column has objects with values that are all the same" do
+        grid = [
+          [x_cell, x_cell, empty],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, empty]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :winner when diagonal has objects with values that are all the same" do
+        grid = [
+          [x_cell, empty, empty],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, x_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :winner
+      end
+
+      it "returns :draw when all the spaces on the board are taken" do
+        grid = [
+          [x_cell, y_cell, x_cell],
+          [y_cell, x_cell, y_cell],
+          [y_cell, x_cell, y_cell]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to eq :draw
+      end
+
+      it "returns false when there is no winner or draw" do
+        grid = [
+          [x_cell, empty, empty],
+          [y_cell, empty, empty],
+          [y_cell, empty, empty]
+        ]
+        board = Board.new(grid: grid)
+        expect(board.game_over).to be_falsey
+      end
+    end
+
   end
-
-  context "#set_cell" do
-    it "updates the value of the cell object at a (x, y) coordinate" do
-      Cat = Struct.new(:value)
-      grid = [[Cat.new("cool"), "", ""], ["", "", ""], ["", "", ""]]
-      board = Board.new(grid: grid)
-      board.set_cell(0, 0, "cell")
-      expect(board.get_cell(0,0).value).to eq "cell"
-    end
-  end
-
-  context "#game_over" do
-    it "returns :winner if winner? is true" do
-      board = Board.new
-      allow(board).to receive(:winner?).and_return(true)
-      expect(board.game_over).to eq :winner
-    end
-
-    it "returns :draw if winner? is false and draw? is true" do
-      board = Board.new
-      allow(board).to receive(:winner?).and_return(false)
-      allow(board).to receive(:draw?).and_return(true)
-      expect(board.game_over).to eq :draw
-    end
-
-    it "returns false if winner? is false and draw? is false" do
-      board = Board.new
-      allow(board).to receive(:winner?).and_return(false)
-      allow(board).to receive(:draw?).and_return(false)
-      expect(board.game_over).to be_falsey
-    end
-  end
-
 end
 end
